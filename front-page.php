2bd041a68233002b5adcf4270a00e3ec0ca1487d
Jucielly <juciellysouzasilva@gmail.com>
2022-06-08 14:29:33 -0300

    feat: add front-page structure

--- front-page.php
+++ front-page.php
@@ -1,8 +1,9 @@
 <?php get_header() ?>
 
+
 <div class="home-container">
     <h3>Tool for your business</h3>
-    <div class="card-containers">
+    <div class="cards-container">
         <div class="card">
             <span class="card-title">Build a experience</span>
             <p class="card-text">When in doubt, wash claws in your leg eat prawns daintily with a claw then lick paws clean wash down</p>
@@ -14,7 +15,7 @@
         <div class="card">
             <span class="card-title">Collaboration</span>
             <p class="card-text">When in doubt, wash claws in your leg eat prawns daintily with a claw then lick paws clean wash down</p>
-        </div>i
+        </div>
     </div>
 </div>
 
