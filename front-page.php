45978fd03c2673f0abcdbfe4a48c12263f8bd724
Jucielly <juciellysouzasilva@gmail.com>
2022-06-08 15:48:14 -0300

    feat: half container in css

--- front-page.php
+++ front-page.php
@@ -1,7 +1,24 @@
 <?php get_header() ?>
-<div class="home-header-container">
 
+
+<div class="home-header">
+    <div class="home-header-texts">
+        <h2 class="home-header-title">
+            Cute, Simple, Fast
+            <br>
+            Coffee...
+        </h2>
+        <p class="home-header-info">
+            When in doubt, wash claws in your leg eat prawns daintily with a claw then lick paws clean wash down
+        </p>
+    </div>
+    <div class="home-header-img">
+        hvhasvduadag
+    </div>
 </div>
+<div class="home-header-half"></div>
+
+
 
 <div class="home-container">
     <h3 class="tools-title">Tool for your business</h3>
